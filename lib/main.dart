7d523247a0f86562b6b87227d9ede6d0c1adcdfb
efren sandoval <efren.sandoval@epn.edu.ec>
2021-10-13 20:48:41 -0500

    putting the list into a card

--- lib/main.dart
+++ lib/main.dart
@@ -24,16 +24,6 @@ class RecipeApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -49,9 +39,21 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: SafeArea(
         child: ListView.builder(
-            itemCount: Recipe.samples.length,
-            itemBuilder: (BuildContext context, int index) =>
-                Text(Recipe.samples[index].label)),
+          itemCount: Recipe.samples.length,
+          itemBuilder: (BuildContext context, int index) =>
+              buildRecipeCard(Recipe.samples[index]),
+        ),
+      ),
+    );
+  }
+
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image(image: AssetImage(recipe.imageUrl)),
+          Text(recipe.label),
+        ],
       ),
     );
   }
